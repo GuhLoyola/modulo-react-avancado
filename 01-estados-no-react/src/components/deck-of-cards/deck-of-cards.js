8ba86d4dae33422f2905fba3de10e29dc8025916
Gustavo Loyola <guga.loyola17@gmail.com>
2023-07-17 21:52:37 -0300

    useState e useEffect implementados no arquivo deck.js

--- 01-estados-no-react/src/components/deck-of-cards/deck-of-cards.js
+++ 01-estados-no-react/src/components/deck-of-cards/deck-of-cards.js
@@ -1,43 +1,43 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { getCards } from "../../services/get-cards";
 import { createDeck } from "../../services/create-deck";
 
-class DeckOfCards extends Component {
-    constructor(){
-        super()
-        this.state = {
-            cards: []
+const DeckOfCards = () => {
+
+    const [deck, setDeck] = useState({
+        cards: []
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const deckId = await createDeck()
+            const data = await getCards(deckId)
+
+            setDeck(
+                {
+                    cards: data.cards
+                }
+            )
         }
-    }
 
-    async componentDidMount(){
-        const deckId = await createDeck()
-        const data = await getCards(deckId)
+        fetchData()
+    }, [])
 
-        this.setState(
-            {
-                cards: data.cards
-            }
-        )
-    }
-
-    render(){
-        return(
-            <section>
-                <ul>
-                    {
-                        this.state.cards.map((card, index) => {
-                            return(
-                                <li key={ index }>
-                                    <img src={ card.image } alt={ card.value }/>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </section>
-        )
-    }
+    return (
+        <section>
+            <ul>
+                {
+                    deck.cards.map((card, index) => {
+                        return (
+                            <li key={index}>
+                                <img src={card.image} alt={card.value} />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
+    )
 }
 
 export default DeckOfCards
